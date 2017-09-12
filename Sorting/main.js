83adfeefeaad401d42a0918cdc1c77783555a5ad
mapcic <mapcic@mail.ru>
2017-09-12 12:53:51 +0300

    Add bubble sort.

--- Sorting/main.js
+++ Sorting/main.js
@@ -9,7 +9,30 @@ function swap(items, left, right) {
 }
 
 class Sort {
-	constructor( set ) {
+	constructor( set ) { 
 		this.set = set;
-	} 
+	}
+
+	swap(items, left, right) {
+		if (left !== right) {
+			var temp = items[left];
+			items[left] = items[right];
+			items[right] = temp; 
+		}
+
+		return;
+	}
+
+	buble() {
+		var swaped = true;
+		while (swaped) {
+			swaped = false;
+			for (var i = 0; i < this.set.length - 1; i++) {
+				if ( this.set[i] > this.set[i+1]) {
+					this.swap(this.set, i, i+1);
+					swaped = true;
+				}
+			}
+		}
+	}
 }
