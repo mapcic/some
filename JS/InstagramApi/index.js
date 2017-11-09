cdc13f31646ff1238a2ce3d93ecfd3d9758420b1
mapcic <mapcic@mail.ru>
2017-11-09 20:49:18 +0300

    Need css

--- JS/InstagramApi/index.js
+++ JS/InstagramApi/index.js
@@ -2,5 +2,9 @@ import './style.css';
 import * as ia from './instagramApi.js';
 var data = require('./instagram_data.json');
 
+console.log(data);
 
-// console.log(data);
+var app = new ia.App();
+
+app.controller.render();
+app.controller.showPosts(data);
--- JS/InstagramApi/instagramApi.js
+++ JS/InstagramApi/instagramApi.js
@@ -15,10 +15,15 @@ export class View {
 		this.elements = [];
 
 		this.html = `<div class="InstagramApi"></div>`;
-		this.element = HtmlElement.createNode(this.html);
+		this.present = HtmlElement.createNode(this.html);
 	}
 
 	addPost(post) {
+		this.present.append(post);
+	}
+
+	getPresent() {
+		return this.present;
 	}
 }
 
@@ -26,12 +31,100 @@ export class Controller {
 	constructor(app) {
 		this.app = app;
 	}
+
+	render() {
+		document.body.append(this.app.view.getPresent());
+	}
+
+	showPosts(instagram) {
+		var app = this.app,
+			model = app.model,
+			view = app.view;
+
+		var params = model.parseInstagramData(instagram);
+
+		// console.log(params);
+		while (true) {
+			let param = params.shift();
+
+			if (!param) {
+				break;
+			}
+
+			let post = new HtmlElementPost(param);
+
+			view.addPost(post.getNode());
+		}
+	}
 }
 
 export class Model {
 	constructor(app) {
 		this.app = app;
 	}
+
+	parseInstagramData(json) {
+		var datas = json.data,
+			now = (new Date()).getTime(),
+			posts = [];
+
+		while (true) {
+			let data = datas.shift(),
+				post = {};
+
+			if (!data) {
+				break
+			}
+
+			post.logo = data.user.profile_picture;
+			post.author = data.user.username;
+			post.location = !data.location? '' : data.location.name;
+			post.date = this.getDifferentTime(now, data.caption? +data.caption.created_time: +data.created_time);
+			post.img = data.images.standard_resolution.url;
+			post.likes = data.likes.count;
+			post.msg =  data.caption? data.caption.text : '';
+
+			console.log(post);
+
+			posts.push(post);
+		}
+
+		return posts;
+	}
+
+	getDifferentTime(now, then) {
+		var dt = new Date(now - then*1000),
+			scnds = dt.getSeconds(),
+			mnts = dt.getMinutes(),
+			hrs = dt.getHours(),
+			ds = dt.getDate(),
+			mnths = dt.getMonth()+1,
+			yrs = dt.getYear() - 70;
+
+		var rsp = `${scnds}s`;
+
+		if (mnts > 0) {
+			rsp = `${mnts}m`;
+		}
+
+		if (hrs > 0) {
+			rsp = `${hrs}h`;
+		}
+
+		if (ds > 0) {
+			rsp = `${ds}d`;
+		}
+
+		if (mnths > 0) {
+			rsp = `${mnths}M`;
+		}
+
+		if (yrs > 0) {
+			rsp = `${yrs}y`;
+		}
+
+		return rsp;
+	} 
 }
 
 class HtmlElement {
@@ -39,26 +132,32 @@ class HtmlElement {
 		this.html = '';
 	}
 
-	static createNode(html) {
-
-	}
+    static createNode(html) {
+        var div = document.createElement('div');
+        
+        div.innerHTML = html;
+        
+        return div.firstChild;
+    }
 
 	getNode() {
-
+		return this.element;
 	}
 }
 
-class HtmlElementPost {
+class HtmlElementPost extends HtmlElement {
 	constructor(params) {
 		super();
 
 		this.html = `<div class="post">
 			<div class="head">
-				<div class="logo">
+				<div class="logo"><a href="https://www.instagram.com/${params.author}/">
 					<img src="${params.logo}">
-				</div>
-				<div class="author">${params.author}</div>
-				<div class="location">${params.location}</div>
+				</a></div>
+				<div class="info">
+					<div class="author">${params.author}</div>
+					<div class="location">${params.location}</div>
+				</div>	
 				<div class="date">${params.date}</div>
 			</div>
 			<div class="img">
@@ -69,5 +168,7 @@ class HtmlElementPost {
 				<div class="msg">${params.msg}</div>
 			</div>
 		</div>`;
+
+		this.element = HtmlElement.createNode(this.html);
 	}
 }
