bdec6d7c10bec00a861283388074c876f6c1ee4d
mapcic <mapcic@mail.ru>
2017-11-14 20:43:50 +0300

    Need Test + Filter row

--- JS/TableClass/presentdata.js
+++ JS/TableClass/presentdata.js
@@ -8,7 +8,17 @@ class PresentData {
 			this.type = type;
 		}
 
+		this.view = document.createElement(`<div></div>`);
 		this.table = document.createElement(`<table></table>`);
+		this.search = document.createElement(`<input type="text"`);
+
+		this.keyTimeout = 0;
+
+		this.view.appendChild(search);
+		this.view.appendChild(table);
+
+		this.rows = [];
+		this.columns = [];
 	}
 
 	seatHeader(titles) {
@@ -24,18 +34,44 @@ class PresentData {
 		this.data.sortByHead(this.head);
 
 		for (var i = 0; i < this.data.length; i++) {
-			let row = (this.data[i].map((val) => `<td>${val}<td>`).join('');
+			let row = (this.data[i].map((val) => `<td>${val}<td>`)).join('');
+			row = `<tr>${row}</tr>`;
 
-			this.table.appendChild(document.createElement(row));
+			this.rows.pop(document.createElement(row));
+			this.table.appendChild(this.rows[this.rows.length-1]);
 		}
 	}
 
-	render() { 
-		
+	render(info) {
 		document.body.appendChild(this.table);
+		this.enableEvents();
 	}
 
+	enableEvents() {
+		this.rows.forEach((val, ind)=>{
+			val.addEventListener('click', event => {
+				this.highlightingRow(event);
+			});
+		}, this);
 
+		this.search.addEventListener('keydown', event => {
+			this.filterRow(event);
+		});
+	}
+
+	highlightingRow(event) {
+		var row = event.currentTarget;
+
+		row.classList.toggle('highlightingRow');
+	}
+
+	filterRow(event) {
+		clearTimeout(this.keyTimeout);
+
+		this.keyTimeout = setTimeout(()=>{
+			console.log(this);
+		});
+	}
 }
 
 class Data {
